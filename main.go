a0de2e48872b7df4b944bf841e89771d94221d95
VERSE <sunnyone0901@gmail.com>
2025-04-28 01:00:10 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/verse91/fuzzysearchvn/fuzzy"
+)
+
+func main() {
+	// Đọc file list.txt
+	file, err := os.Open("list.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var items []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		items = append(items, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Nhập từ khóa
+	var query string
+	fmt.Print("Nhập từ khóa: ")
+	fmt.Scanln(&query)
+
+	// Tìm kiếm
+	results := fuzzyvn.Find(query, items)
+
+	// In kết quả
+	fmt.Println("Kết quả tìm được:")
+	for _, res := range results {
+		fmt.Println(res)
+	}
+}
